f82e2f597418c23dc31e6384098d15d9870e8ba8
Krishmakumari <118874025+Krishmakumari@users.noreply.github.com>
2024-07-22 18:12:34 +0530

    Add files via upload

---
+++ Week2/lecture_18/first-app/src/App.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import TODOScreen from "./screens/TODOScreen";
+
+class App extends Component{
+    render(){
+      return <TODOScreen/>;
+    }
+
+}
+export default App;
---
+++ Week2/lecture_18/first-app/src/screens/TODOScreen.js
@@ -0,0 +1,8 @@
+import { Component } from "react";
+class TODOScreen extends Component{
+    render(){
+        return <h1 className="ui heading center">To 
+        Do List</h1>;
+    }
+}
+export default TODOScreen;
