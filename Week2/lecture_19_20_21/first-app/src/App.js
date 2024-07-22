a41da5537d86436c81521c2e6eefc9da4ad0b3ae
Krishmakumari <118874025+Krishmakumari@users.noreply.github.com>
2024-07-22 20:12:09 +0530

    Add files via upload

---
+++ Week2/lecture_19_20_21/first-app/src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import TODOScreen from "./screens/TODOScreen";
+
+// class App extends Component{
+//     render(){
+//       return <TODOScreen/>;
+//     }
+
+// }
+const App=()=>{
+  return <TODOScreen/>
+}
+export default App;
---
+++ Week2/lecture_19_20_21/first-app/src/screens/TODOScreen.js
@@ -0,0 +1,36 @@
+import {useState} from "react";
+import Task from "../components/Task";
+const TODOScreen=()=>{
+
+    const [tasklist,setTasklist]=useState([]);
+    return (
+                <div className="screen">
+                <h1 className="ui heading center">To 
+                Do List</h1>
+                <div>
+                    <button
+                     onClick={(e)=>{
+                        setTasklist([...tasklist,
+                            {
+                            title: "Go to gym",
+                            description: "Going to the gym is good for muscle growth.",
+                            createdDate: new Date(),
+                        },
+                    ]);
+                    }}
+                     className="ui secondary button">
+                        Add task
+                        </button>
+                        <section>
+                     <div class="ui cards">
+                     {tasklist.map((task,index)=> (
+                        <Task task={task}  key={index} />    
+                        ))}
+                </div>
+                </section>
+                </div>
+                </div>
+                );
+}
+
+export default TODOScreen;
